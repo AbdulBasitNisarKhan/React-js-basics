5841bee764d1bfd08da9f80656448ac933ea18ae
AbdulBasitNisarKhan <khanbasit187@gmail.com>
2024-05-23 14:31:46 +0500

    react js

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Message from "./messga"
+
+
+
+function App() {
+
+
+  return (
+    // <h1>Learning React with Vite| Adul basit Nisar Khan</h1>
+    <>
+      <h1>Message function</h1>
+      <Message />
+
+    </>
+
+  )
+}
+
+export default App
---
+++ 01viteReact/src/messga.jsx
@@ -0,0 +1,7 @@
+function Message() {
+    return (
+        <h3>Welcom to React web dev</h3>
+    )
+}
+
+export default Message
